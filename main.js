469a3310cef4c5fdad5de8e9eda3fad8c79fd734
shyamtala003 <shyamtala003@gmail.com>
2022-08-30 17:08:34 +0530

    add login modal and fix hero section

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // toggle dropdown
-let dropDownBtn= document.querySelectorAll('.dropdown-btn');
+let dropDownBtn = document.querySelectorAll('.dropdown-btn');
 
 dropDownBtn.forEach((Element)=>{
     Element.addEventListener("click",()=>{
@@ -14,13 +14,55 @@ dropDownBtn.forEach((Element)=>{
 
 // navbar open/close
 
-let Navabar=document.getElementById('sidebar');
-let menuIcon=document.getElementById('menu__icon');
+let Navabar = document.getElementById('sidebar');
+let menuIcon = document.getElementById('menu__icon');
 
-menuIcon.addEventListener("click",()=>{
+menuIcon.addEventListener("click", ()=>{
     Navabar.classList.toggle("sidebar__toggle");
-    setTimeout(()=>{
+    setTimeout( () => {
         menuIcon.getAttribute("src")==="./images/nav-icon.png"? menuIcon.setAttribute("src","./images/close.png") : menuIcon.setAttribute("src","./images/nav-icon.png");
-    },200)
+    }, 200)
 })
 
+
+// code for modal open/close
+
+let Modal = document.getElementById('modal-login');
+let ModalClose = document.getElementById('modal__close');
+
+let loginBtn1 = document.getElementById('login-btn');
+let loginBtn2 = document.getElementById('join-btn');
+
+
+loginBtn1.addEventListener("click", () => {
+    Modal.classList.toggle("modal-active");
+})
+
+loginBtn2.addEventListener("click", () => {
+    Modal.classList.toggle("modal-active");
+})
+
+ModalClose.addEventListener("click", () => {
+    Modal.classList.remove("modal-active")
+})
+
+
+let modalTitle=document.getElementById('modal-title');
+let modalBtn=document.getElementById('text-toggle');
+let modalBtn2= document.getElementById('modal-btn-text');
+
+modalBtn.addEventListener("click", () => {
+    
+    if(modalBtn.innerHTML=="Login")
+    {
+        modalBtn2.innerText="Don't have an account?";
+        modalBtn.innerText="Register";
+        modalTitle.innerText="Register";
+    }else
+    {
+        modalBtn2.innerText="Already have an account?";
+        modalBtn.innerText="Login";
+        modalTitle.innerText="Login";
+    }
+})
+console.log(modalBtn2.innerText);
