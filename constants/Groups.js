6dc3e1d4f24fb8e3a3e3dade941a992e0a6a1e8a
shyamtala003 <shyamtala003@gmail.com>
2022-09-04 12:02:26 +0530

    constants/NavBar.js added

--- constants/Groups.js
+++ constants/Groups.js
@@ -1,93 +1,110 @@
+
+// Note:currently given propertie(tags) inside each array object is not in used 
 let group=[
     {
         groupName:"Javascript Community",
         groupDesc:"Learn and help from each other. It doesn't matter from wh...",
         groupCoverImage:"./images/cards/s1.jpg",
-        numOfPeople:248
+        numOfPeople:248,
+        tags:["General","Angular","React js","Backend with JS"]
     },
     {
         groupName:"Python Community",
         groupDesc:"Learn and help from each other. It doesn't matter from wh...",
         groupCoverImage:"./images/cards/s2.png",
-        numOfPeople:123
+        numOfPeople:123,
+        tags:["General","WebDev Py","Machine Learning Py"]
     },
     {
         groupName:"Java Community",
         groupDesc:"Learn and help from each other. It doesn't matter from wh...",
         groupCoverImage:"./images/cards/s3.png",
-        numOfPeople:112
+        numOfPeople:112,
+        tags:["General"]
     },
     {
         groupName:"C/CPP Community",
         groupDesc:"Learn and help from each other. It doesn't matter from wh...",
         groupCoverImage:"./images/cards/s4.png",
-        numOfPeople:112
+        numOfPeople:112,
+        tags:["General","C Programing","CPP"]
     },
     {
         groupName:"System Design Community",
         groupDesc:"Learn and help from each other. It doesn't matter from wh...",
         groupCoverImage:"./images/cards/s5.png",
-        numOfPeople:43
+        numOfPeople:43,
+        tags:["General"]
     },
     {
         groupName:"Mobile developer Community",
         groupDesc:"Learn and help from each other. It doesn't matter from wh...",
         groupCoverImage:"./images/cards/s6.png",
-        numOfPeople:89
+        numOfPeople:89,
+        tags:["General","react native","flutter","ios","android"]
     },
     {
         groupName:"Front-End Community",
         groupDesc:"Learn and help from each other. It doesn't matter from wh...",
         groupCoverImage:"./images/cards/s7.png",
-        numOfPeople:1289
+        numOfPeople:1289,
+        tags:["General","HTML & CSS","Tailwind","Bootstrap","Material"]
     },
     {
         groupName:"Data Structure & Algorithm",
         groupDesc:"Learn and help from each other. It doesn't matter from wh...",
         groupCoverImage:"./images/cards/s8.png",
-        numOfPeople:89
+        numOfPeople:89,
+        tags:["General","DSA With Python","DSA with Java","DSA With JavaScript"]
     },
     {
         groupName:"Database Community",
         groupDesc:"Learn and help from each other. It doesn't matter from wh...",
         groupCoverImage:"./images/cards/s9.png",
-        numOfPeople:34
+        numOfPeople:34,
+        tags:["General","Sql","NoSql"]
     },
     {
         groupName:"DevOps Community",
         groupDesc:"Learn and help from each other. It doesn't matter from wh...",
         groupCoverImage:"./images/cards/s10.png",
-        numOfPeople:34
+        numOfPeople:34,
+        tags:["General","Linux","Docker","Ansible","Kubernetes"]
     },
     {
         groupName:"Cloud Community",
         groupDesc:"Learn and help from each other. It doesn't matter from wh...",
         groupCoverImage:"./images/cards/s11.png",
-        numOfPeople:32
+        numOfPeople:32,
+        tags:["General","Azure","Aws","GCP"]
     },
     {
         groupName:"Campus Tech leaders",
         groupDesc:"Learn and help from each other. It doesn't matter from wh...",
         groupCoverImage:"./images/cards/s12.png",
-        numOfPeople:12
+        numOfPeople:12,
+        tags:["General","Batch-2"]
     },
     {
         groupName:"Software Testing Community",
         groupDesc:"Learn and help from each other. It doesn't matter from wh...",
         groupCoverImage:"./images/cards/s13.png",
-        numOfPeople:43
+        numOfPeople:43,
+        tags:["General","JavaScript -QA","Cypress","Manual Testing"]
     },
     {
         groupName:"Backend devs Community",
         groupDesc:"Learn and help from each other. It doesn't matter from wh...",
         groupCoverImage:"./images/cards/s14.png",
-        numOfPeople:435
+        numOfPeople:435,
+        tags:["General","JS Backend","JAVA Backend","PHP"]
     },
     {
         groupName:"Javascript paid bootcamp | Pro",
         groupDesc:"Learn and help from each other. It doesn't matter from wh...",
         groupCoverImage:"./images/cards/s15.png",
-        numOfPeople:435
+        numOfPeople:435,
+        tags:["General","Chat"]
     },
 
 ];
@@ -122,6 +139,3 @@ group.forEach((Element) => {
 })
 
 
-// code for displaying number of group in community
-let badge=document.querySelector(".badge");
-badge.innerHTML=group.length;
---
+++ constants/NavBar.js
@@ -0,0 +1,32 @@
+let mainPages=[
+    {
+        pageName:"Home",
+        icon:"home-outline",
+        link:"#"
+    },
+    {
+        pageName:"explore groups",
+        icon:"navigate-circle-outline",
+        link:"#"
+    },
+    {
+        pageName:"events",
+        icon:"calendar-outline",
+        link:"#"
+    }
+]
+
+let navbarLinks=document.querySelectorAll(".side__navbar");
+let mainLinks=navbarLinks[0];
+
+mainPages.forEach(Element => {
+    mainLinks.innerHTML+=`
+    <a href=${Element.link}>
+        <li>
+            <ion-icon name=${Element.icon}></ion-icon> ${Element.pageName}
+        </li>
+    </a>
+    `;
+})
+
+
